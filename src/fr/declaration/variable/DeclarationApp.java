47cbe6411a10fc82fbfced279576a040baca6ac2
Carolanne Yvon <elolithia@gmail.com>
2022-10-06 16:40:22 +0200

    DeclarationApp

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,28 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// DECLAREZ LES VARIABLES
+		byte a = 115;
+		short b = 31670;
+		int c = 178;
+		long d = 1988L;
+		float e = 10.5F;
+		double f = 34;
+		char g = 'A';
+		boolean h = true;
+		String i = "Et voilà";
+		
+		// AFFICHAGE DE LA VALEUR D’UNE VARIABLE
+		System.out.println(i);
+		
+		// Retour à la ligne dans une chaine de caractère
+		String randomString = "Voici le résultat d’un calcul :\n1+5=6";
+		System.out.println(randomString);
+		
+		// Utilisation du Debugger
+
+	}
+
+}
